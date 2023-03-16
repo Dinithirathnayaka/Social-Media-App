d87682867dde5f11862d44bf0eac89bf9bfa7c49
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-03-16 22:27:04 +0530

    modify frontend

--- frontend/src/components/Rightbar.jsx
+++ frontend/src/components/Rightbar.jsx
@@ -1,3 +1,175 @@
 export default function Rightbar() {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/5.png" alt="" />
+          <span className="birthdayaText">
+            <b>Supun Rajaguru</b> & <b>3 other friends</b> have a birthdaya
+            today.Wish them.
+          </span>
+        </div>
+        <img className="rightbarAd" src="/assets/6.jpeg" alt="" />
+        <h4 className="rightbarTitle">OnlionFriends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Anne Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
